35937f7419312bb7e4d19b7de4de7855681d81e0
Patryk <patryk.karpinski@outlook.com>
2018-07-30 00:13:17 +0200

    - angular material installed, - bug of angular slider fixed, - navbar implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { GestureConfig, MatCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatSliderModule,
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatIconModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CategoryCreateComponent } from './categories/category-create/category-create.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CategoryCreateComponent
-  ],
+  declarations: [AppComponent, CategoryCreateComponent, NavbarComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/categories/category-create/category-create.component.ts
+++ src/app/categories/category-create/category-create.component.ts
@@ -2,7 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-category-create',
-  templateUrl: './category-create.component.html'
-})export class CategoryCreateComponent {
+  templateUrl: './category-create.component.html',
+  styleUrls: ['./category-create.component.css']
+})
+export class CategoryCreateComponent {
+  onAddCategory() {
+    console.log('added');
+  }
 
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 0) {
+      return value + '%';
+    }
+
+    return value;
+  }
 }
